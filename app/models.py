4150c931bb717255f72477f928ba36e222f0ee2b
Dmitry Timofeev <dimkat@gmail.com>
2013-01-22 13:48:13 +0400

    Skeleton for LogEntry model.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,28 @@
 # -*- coding: utf-8 -*-
 
 class LogEntry:
-    pass
+    """Main class for new log entries.
+
+    Keyword arguments:
+    owner - person ot system, who create this entry (required)
+    datetimestamp - date and time, when entry was created (required)
+    level - attentiveness level (required):
+        - critical – for errors that lead to termination
+        - error – for errors that occur, but are handled
+        - warning – for exceptional circumstances that might not be errors
+        - notice – for non-error messages you usually want to see
+        - info – for messages you usually don’t want to see
+        - debug – for debug messages
+    data - main message body (required)
+    tags - tags list for extended search
+
+    """
+    def __init__(self, level, datetimestamp, owner, data, tags=[]):
+        self.level = level
+        self.datetimestamp = datetimestamp
+        self.owner = owner
+        self.data = data
+        self.tags = tags
+
+    def save(self):
+        pass
